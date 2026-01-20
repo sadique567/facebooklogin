1fea38b2e226447fb59ea7f96cd5529ab7ea6481
sadique567 <mohdsadique008@gmail.com>
2026-01-20 19:08:04 +0530

    twitter login working

--- lib/app/module/home/controller.dart
+++ lib/app/module/home/controller.dart
@@ -1,90 +1,156 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // 🔥 Observable user
+  Rx<User?> currentUser = Rx<User?>(null);
 
+  @override
+  void onInit() {
+    super.onInit();
+    currentUser.value = _auth.currentUser;
+  }
 
-
-
-  Future<void> loginWithTwitter() async {    
+  // Twitter Login
+  Future<void> loginWithTwitter() async {
     try {
-      // Trigger the sign-in flow
+      if (_auth.currentUser != null) {
+        Get.snackbar("Already Logged In", "Please logout first");
+        return;
+      }
+
       final twitterProvider = TwitterAuthProvider();
+      twitterProvider.setCustomParameters({
+        'force_login': 'true', // force account chooser
+        'lang': 'en',
+      });
 
-      // If you need to force OAuth 1.0 popup
-      twitterProvider.setCustomParameters({'lang': 'en'});
+      UserCredential credential =
+          await _auth.signInWithProvider(twitterProvider);
 
-      // Firebase sign in with provider
-      final UserCredential twitterUser = await FirebaseAuth.instance
-          .signInWithProvider(twitterProvider);
+      currentUser.value = credential.user;
 
-      final user = twitterUser.user;
-      print("user : ${user!.displayName}");
-
-      // Fetch Twitter email (if app has 'Request email from users' enabled)
-      final String? email = user.email;
-      final String? name = user.displayName;
-      final String? photo = user.photoURL;
-
-      debugPrint("TWITTER UID: ${user.uid}");
-      debugPrint("NAME: $name");
-      debugPrint("EMAIL: $email");
-      debugPrint("PHOTO: $photo");
-
-      // Your login / signup logic
-      if (email != null) {
-        print("Email is not null");
-        // isLogin
-        //     ? login(email: email, pass: '', isEmail: 2)
-        //     : signUp(email: email, pass: '', isSocial: true);
-      } else {
-        Get.snackbar(
-          "Email Missing",
-          "Twitter did not return an email. Enable email permission in Twitter Developer Portal.",
-        );
-      }
     } catch (e) {
-      print("telegram login Error : ${e.toString()}");
-      if (e.toString().contains("web-context-canceled")) {
-        Get.snackbar("Cancelled", "Twitter login cancelled by user");
-      } else {
-        Get.snackbar("Error 1", e.toString());
-      }
+      Get.snackbar("Login Error", e.toString());
     }
   }
 
-
-  Future<void> logoutFromFacebook() async {
-    try {
-      // 1️⃣ Firebase logout
-      await FirebaseAuth.instance.signOut();
-
-      // 2️⃣ Facebook logout (VERY IMPORTANT)
-      await FacebookAuth.instance.logOut();
-
-      // 3️⃣ Clear local storage (SharedPreferences)
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.clear();
-
-      print("FULL LOGOUT SUCCESS");
-    } catch (e) {
-      print("LOGOUT ERROR: $e");
-    }
-
-    // try {
-    //   // Firebase logout
-    //   await FirebaseAuth.instance.signOut();
-
-    //   // Facebook logout
-    //   await FacebookAuth.instance.logOut();
-
-    //   print("LOGOUT SUCCESS");
-    // } catch (e) {
-    //   print("LOGOUT ERROR: $e");
-    // }
+  // Logout
+  Future<void> logout() async {
+    await _auth.signOut();
+    currentUser.value = null;
   }
 }
+
+
+
+
+
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:get/get.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// class HomeController extends GetxController {
+
+
+//   Future<void> loginWithTwitter() async {
+//     try {
+//       // Trigger the sign-in flow
+//       final twitterProvider = TwitterAuthProvider();
+
+//       // If you need to force OAuth 1.0 popup
+//       twitterProvider.setCustomParameters({
+//         'force_login': 'true',
+//         'lang': 'en',
+//       });
+
+//       // Firebase sign in with provider
+//       final UserCredential twitterUser = await FirebaseAuth.instance
+//           .signInWithProvider(twitterProvider);
+//       print("Logged in UID: ${twitterUser.user?.uid}");
+//       final user = twitterUser.user;
+//       print("user : ${user!.displayName}");
+
+//       // Fetch Twitter email (if app has 'Request email from users' enabled)
+//       final String? email = user.email;
+//       final String? name = user.displayName;
+//       final String? photo = user.photoURL;
+
+//       debugPrint("TWITTER UID: ${user.uid}");
+//       debugPrint("NAME: $name");
+//       debugPrint("EMAIL: $email");
+//       debugPrint("PHOTO: $photo");
+
+//       // Your login / signup logic
+//       if (email != null) {
+//         print("Email is not null");
+//         // isLogin
+//         //     ? login(email: email, pass: '', isEmail: 2)
+//         //     : signUp(email: email, pass: '', isSocial: true);
+//       } else {
+//         Get.snackbar(
+//           "Email Missing",
+//           "Twitter did not return an email. Enable email permission in Twitter Developer Portal.",
+//         );
+//       }
+//     } catch (e) {
+//       print("Twitter  Error : ${e.toString()}");
+//       if (e.toString().contains("web-context-canceled")) {
+//         Get.snackbar("Cancelled", "Twitter login cancelled by user");
+//       } else {
+//         print("Twitter Error : ${e.toString()}");
+
+//         Get.snackbar("Error 1", e.toString());
+//       }
+//     }
+//   }
+
+//   Future<void> logoutFromTwitter() async {
+//     try {
+//       // Firebase sign out
+//       await FirebaseAuth.instance.signOut();
+
+//       debugPrint("Twitter + Firebase logout success");
+
+//       // Navigate to login screen (optional but recommended)
+//       // Get.offAll(() => LoginScreen());
+//     } catch (e) {
+//       debugPrint("Logout Error: $e");
+//       Get.snackbar("Logout Error", e.toString());
+//     }
+//   }
+
+//   Future<void> logoutFromFacebook() async {
+//     try {
+//       // 1️⃣ Firebase logout
+//       await FirebaseAuth.instance.signOut();
+
+//       // 2️⃣ Facebook logout (VERY IMPORTANT)
+//       await FacebookAuth.instance.logOut();
+
+//       // 3️⃣ Clear local storage (SharedPreferences)
+//       final prefs = await SharedPreferences.getInstance();
+//       await prefs.clear();
+
+//       print("FULL LOGOUT SUCCESS");
+//     } catch (e) {
+//       print("LOGOUT ERROR: $e");
+//     }
+
+//     // try {
+//     //   // Firebase logout
+//     //   await FirebaseAuth.instance.signOut();
+
+//     //   // Facebook logout
+//     //   await FacebookAuth.instance.logOut();
+
+//     //   print("LOGOUT SUCCESS");
+//     // } catch (e) {
+//     //   print("LOGOUT ERROR: $e");
+//     // }
+//   }
+// }
--- lib/app/module/home/view.dart
+++ lib/app/module/home/view.dart
@@ -1,58 +1,194 @@
-import 'package:fblogin/app/module/home/controller.dart';
-import 'package:fblogin/app/service/facebookOAuthService.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'controller.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              await controller.logoutFromFacebook();
-              Get.snackbar("Success", "Logged out successfully");
+      appBar: AppBar(title: const Text("Twitter Login"), centerTitle: true),
+      body: Obx(() {
+        final user = controller.currentUser.value;
 
-              // agar login screen pe bhejna ho
-              // Get.offAllNamed(Routes.LOGIN);
-            },
+        // 🔴 NOT LOGGED IN
+        if (user == null) {
+          return Center(
+            child: ElevatedButton(
+              onPressed: controller.loginWithTwitter,
+              child: const Text("Login with Twitter"),
+            ),
+          );
+        }
+
+        // 🟢 LOGGED IN
+        return Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /// PROFILE IMAGE
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: user.photoURL != null
+                    ? NetworkImage(user.photoURL!)
+                    : null,
+                child: user.photoURL == null
+                    ? const Icon(Icons.person, size: 40)
+                    : null,
+              ),
+
+              const SizedBox(height: 16),
+
+              /// NAME
+              Text(
+                user.displayName ?? "No Name",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              /// EMAIL
+              Text(user.email ?? "Email not provided"),
+
+              const SizedBox(height: 20),
+
+              /// FULL USER DATA CARD
+              // Card(
+              //   elevation: 4,
+              //   shape: RoundedRectangleBorder(
+              //     borderRadius: BorderRadius.circular(12),
+              //   ),
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(16),
+              //     child: Column(
+              //       crossAxisAlignment: CrossAxisAlignment.start,
+              //       children: [
+
+              //         _row("UID", user.uid),
+              //         _row("Provider ID", user.providerData.first.providerId),
+              //         _row("Display Name", user.displayName),
+              //         _row("Email", user.email),
+              //         _row("Phone", user.phoneNumber),
+              //         _row("Photo URL", user.photoURL),
+              //         _row("Email Verified", user.emailVerified.toString()),
+              //         _row("Anonymous", user.isAnonymous.toString()),
+              //         _row(
+              //           "Account Created",
+              //           user.metadata.creationTime.toString(),
+              //         ),
+              //         _row(
+              //           "Last Sign In",
+              //           user.metadata.lastSignInTime.toString(),
+              //         ),
+
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              const SizedBox(height: 25),
+
+              /// LOGOUT BUTTON
+              ElevatedButton(
+                onPressed: controller.logout,
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                child: const Text("Logout"),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        );
+      }),
+    );
+  }
+
+  /// 🔹 Reusable row widget
+  Widget _row(String title, String? value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ElevatedButton(
-            onPressed: () async {
-              controller.loginWithTwitter();
-            },
-            child: Text("🪁 Login with Twitter"),
+          Expanded(
+            flex: 3,
+            child: Text(
+              "$title :",
+              style: const TextStyle(fontWeight: FontWeight.w600),
+            ),
           ),
-          SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: () async {
-              final userCredential = await signInWithFacebook();
-              if (userCredential != null) {
-                print('Login success: ${userCredential.user?.email}');
-              } else {
-                print('Login failed');
-              }
-              // signInWithFacebook();
-              // Get.toNamed(Routes.FACEBOOK_LOGIN);
-            },
-            child: Text("Login with facebookcee"),
-          ),
+          Expanded(flex: 5, child: Text(value ?? "N/A")),
         ],
       ),
     );
   }
 }
 
-// flutter run --verbose
+// import 'package:fblogin/app/module/home/controller.dart';
+// import 'package:fblogin/app/service/facebookOAuthService.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:get/get.dart';
+
+// class HomeView extends GetView<HomeController> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home Screen"),
+//         centerTitle: true,
+//         automaticallyImplyLeading: false,
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.logout),
+//             onPressed: () async {
+//               await controller.logoutFromFacebook();
+//               Get.snackbar("Success", "Logged out successfully");
+//               // agar login screen pe bhejna ho
+//               // Get.offAllNamed(Routes.LOGIN);
+//             },
+//           ),
+//         ],
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Row(
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   controller.loginWithTwitter();
+//                 },
+//                 child: Text("🪁 Login with Twitter"),
+//               ),
+//               SizedBox(width: 25),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   controller.logoutFromTwitter();
+//                 },
+//                 child: Text("Logout"),
+//               ),
+//             ],
+//           ),
+//           SizedBox(height: 15),
+//           ElevatedButton(
+//             onPressed: () async {
+//               final userCredential = await signInWithFacebook();
+//               if (userCredential != null) {
+//                 print('Login success: ${userCredential.user?.email}');
+//               } else {
+//                 print('Login failed');
+//               }
+//               // signInWithFacebook();
+//               // Get.toNamed(Routes.FACEBOOK_LOGIN);
+//             },
+//             child: Text("Login with facebook"),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// // flutter run --verbose
